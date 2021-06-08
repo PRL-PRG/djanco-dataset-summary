ae9e9099ec99c2bd7412adbcb964603e7f44ea2c
Konrad Siek <konrad.siek@gmail.com>
2021-06-08 13:55:52 +0200

    Losening restrictions.

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ use djanco_ext::*;
 /**
  * Gathers basic information about projects in the dataset and save it to `projects.csv`.
  */
-#[djanco(May, 2021, subsets(All))]
+#[djanco]
 pub fn project_summary(database: &Database, _log: &Log, output: &Path) -> Result<(), std::io::Error>  {
     database.projects().into_csv_in_dir(output, "projects.csv")
 }
